6d78f69c56ca05d3e4ede9334b5420b3d4aba6c5
Default User <laurence.a.maddox@gmail.com>
2017-09-26 09:17:01 -0500

    backup

--- autogen.sh
+++ autogen.sh
@@ -1,28 +1,4 @@
 #! /bin/bash
 set -exo nounset
 
-if [ $# -ne 0 ] ; then
-	git pull origin master
-	git submodule foreach git fetch
-	git submodule foreach git checkout master
-	git submodule foreach git merge origin/master
-	if [ -f Makefile ] ; then
-		make distclean
-	fi
-fi
-
-autoreconf --verbose --force --install --include=m4
-
-if [ $# -eq 0 ] ; then exit 0
-else
-	test "x$1" == xmake
-	DIR="$(readlink -f `pwd`)"
-	rm -fr ../DOS-build
-	mkdir ../DOS-build
-	cd ../DOS-build
-	#"$DIR/configure" "--prefix=$HOME"
-	"$DIR/configure" --prefix=/home/dos
-	make
-	#make install
-	sudo -u dos make -j1 install
-fi
+autoreconf --verbose --force --install
